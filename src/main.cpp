d9c008c3a751e0a4febddf238770d8ba66641569
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2023-05-02 09:13:28 +0900

    :heartbeat: リファクタリング

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,72 @@
 #include <M5Unified.h>
 
 
-constexpr const char* DK_DISPLAY_IMAGE_PATH = "/image.png";
-constexpr const uint32_t DK_DISPLAY_IMAGE_SIZE = 8192;
-constexpr const uint8_t DK_SENSOR_INPUT_PIN = 1;
-constexpr const uint8_t DK_SENSOR_TIMER_SIZE = 10;
+const char* D_DISPLAY_IMAGE_PATH = "/image.png";
+const int D_DISPLAY_IMAGE_SIZE = 8192;
+const int D_SENSOR_INPUT_PIN = 1;
+const int D_SENSOR_TIMER_SIZE = 4;
 
 
-M5Canvas dkDisplayCanvas(&M5.Display);
-uint8_t dkDisplayImage[DK_DISPLAY_IMAGE_SIZE];
-volatile bool dkSensorIsActive = false;
-bool dkSensorWasActive = true;
-uint8_t dkSensorRate = 0;
-uint8_t dkSensorCounter = 0;
-uint32_t dkSensorTimer[DK_SENSOR_TIMER_SIZE] = {};
+volatile bool gDisplayIsPulsed = true;
+M5Canvas gDisplayCanvas(&M5.Display);
+uint8_t gDisplayImage[D_DISPLAY_IMAGE_SIZE] = {};
+volatile bool gSensorIsMeasured = true;
+int gSensorRate = 0;
+int gSensorCounter = 0;
+unsigned long gSensorTimer[D_SENSOR_TIMER_SIZE] = {};
 
 
 /** ディスプレイのタスク */
 void taskDisplay(void* pvParameters) {
-  uint32_t backgroundColor = 0x7ECDF6;
-  uint32_t textColor = 0xEE4096;
-  int32_t displayW = M5.Display.width();
-  int32_t displayH = M5.Display.height();
   while (true) {
-    if (dkSensorIsActive != dkSensorWasActive) {
-      int32_t imageX = dkSensorIsActive ? 0 : -2;
-      int32_t imageY = dkSensorIsActive ? 0 : 2;
-      int32_t cursorX = 126 - 12 * String(dkSensorRate).length();
-      int32_t cursorY = 4;
-      dkDisplayCanvas.createSprite(displayW, displayH);
-      dkDisplayCanvas.fillRect(0, 0, displayW, displayH, backgroundColor);
-      dkDisplayCanvas.setCursor(cursorX, cursorY);
-      dkDisplayCanvas.setTextColor(textColor);
-      dkDisplayCanvas.setTextSize(2);
-      dkDisplayCanvas.printf("%s\n", dkSensorRate > 0 ? String(dkSensorRate) : "-");
-      dkDisplayCanvas.drawPng(dkDisplayImage, DK_DISPLAY_IMAGE_SIZE, imageX, imageY);
-      dkDisplayCanvas.pushSprite(0, 0);
+    if (gDisplayIsPulsed) {
+      auto imageX = gSensorIsMeasured ? 0 : -2;
+      auto imageY = gSensorIsMeasured ? 0 : 2;
+      auto sensorText = gSensorRate ? String(gSensorRate) : String("-");
+      auto sensorX = 126 - 12 * sensorText.length();
+      gDisplayCanvas.createSprite(128, 128);
+      gDisplayCanvas.fillRect(0, 0, 128, 128, 0x7ECDF6UL);
+      gDisplayCanvas.setCursor(sensorX, 4);
+      gDisplayCanvas.setTextColor(0xEE4096UL);
+      gDisplayCanvas.setTextSize(2);
+      gDisplayCanvas.printf("%s\n", sensorText);
+      gDisplayCanvas.drawPng(gDisplayImage, D_DISPLAY_IMAGE_SIZE, imageX, imageY);
+      gDisplayCanvas.pushSprite(0, 0);
       Serial.printf("[INFO] Display updated.\n");
     }
-    dkSensorWasActive = dkSensorIsActive;
-    dkSensorIsActive = false;
+    if (!gSensorIsMeasured) {
+      gDisplayIsPulsed = false;
+    }
+    gSensorIsMeasured = false;
     // Serial.printf("[DEBUG] Display task done.\n");
     delay(100);
   }
 }
 
 
+/** センサーのタスク */
+void taskSensor() {
+  auto previousTime = gSensorTimer[gSensorCounter];
+  auto currentTime = millis();
+  if (previousTime > 0) {
+    auto rate = (int)((60 * 1000 * D_SENSOR_TIMER_SIZE) / (currentTime - previousTime));
+    gSensorRate = rate;
+    Serial.printf("<- %d\n", rate);
+    Serial.printf("[INFO] Sensor rate measured. %d\n", rate);
+  }
+  gSensorTimer[gSensorCounter++] = currentTime;
+  gSensorCounter %= D_SENSOR_TIMER_SIZE;
+  gSensorIsMeasured = true;
+  gDisplayIsPulsed =  true;
+  // Serial.printf("[DEBUG] Sensor task done.\n");
+}
+
+
 /** ディスプレイの初期設定 */
 void beginDisplay() {
   SPIFFS.begin();
-  File file = SPIFFS.open(DK_DISPLAY_IMAGE_PATH);
-  file.read(dkDisplayImage, DK_DISPLAY_IMAGE_SIZE);
+  auto file = SPIFFS.open(D_DISPLAY_IMAGE_PATH);
+  file.read(gDisplayImage, D_DISPLAY_IMAGE_SIZE);
   file.close();
   M5.Display.setBrightness(64);
   xTaskCreatePinnedToCore(taskDisplay, "taskDisplay", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
@@ -60,26 +76,9 @@ void beginDisplay() {
 }
 
 
-/** センサーのタスク */
-void taskSensor() {
-  uint32_t previous = dkSensorTimer[dkSensorCounter];
-  uint32_t current = millis();
-  if (previous > 0) {
-    uint8_t rate = (60 * 1000 * DK_SENSOR_TIMER_SIZE) / (current - previous);
-    dkSensorRate = rate;
-    Serial.printf("<- %d\n", rate);
-    Serial.printf("[INFO] Sensor rate calclated. %d\n", rate);
-  }
-  dkSensorTimer[dkSensorCounter] = current;
-  dkSensorCounter = (dkSensorCounter + 1) % DK_SENSOR_TIMER_SIZE;
-  dkSensorIsActive = true;
-  // Serial.printf("[DEBUG] Sensor task done.\n");
-}
-
-
 /** センサーの初期設定 */
 void beginSensor() {
-  attachInterrupt(digitalPinToInterrupt(DK_SENSOR_INPUT_PIN), taskSensor, RISING);
+  attachInterrupt(digitalPinToInterrupt(D_SENSOR_INPUT_PIN), taskSensor, RISING);
   // Serial.printf("[DEBUG] Sensor begun.\n");
 }
 
@@ -89,25 +88,24 @@ void setup() {
   M5.begin();
   beginDisplay();
   beginSensor();
-  Serial.printf("[INFO] Setup done.\n");
+  // Serial.printf("[DEBUG] M5 begun.\n");
 }
 
 
-/** シリアル通信の監視 */
-void observeSerial() {
+/** シリアルの状態更新 */
+void updateSerial() {
   if (Serial.available()) {
-    String message = Serial.readString();
-    if (message.startsWith("ping")) {
-      Serial.printf("pong\n");
-    }
+    auto message = Serial.readString();
     Serial.printf("[INFO] Serial message received. %s\n");
   }
-  // Serial.printf("[DEBUG] Sensor observed.\n");
+  // Serial.printf("[DEBUG] Serial updated.\n");
 }
 
 
 /** ループ */
 void loop() {
   M5.update();
-  observeSerial();
+  updateSerial();
+  // Serial.printf("[DEBUG] M5 updated.\n");
+  delay(100);
 }
